8399c781b2634e9d087ba08d06ab67fecb1579c2
hendrichhrm <hendrichherdanto16@gmail.com>
2024-05-31 14:03:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
+const express = require('express');
 const mqtt = require('mqtt');
 
-const option = {
-    username: 'hendrich@hivemq',
-    password: "Admin_project1"
-}
-const client = mqtt.connect('broker.hivemq.com', option);
+const app = express();
+const port = 8884;
+
+// Konfigurasi MQTT
+const mqttBrokerUrl = 'mqtt://broker.hivemq.com:8884';
+const mqttOptions = {
+  username: 'hendrich@hivemq',
+  password: 'Admin_project1',
+  protocol: 'mqtts'
+};
+const topicSub = 'skripsi/byhendrich/subscribe';
+const topicPub = 'skripsi/byhendrich/publish';
+
+// Membuat client MQTT
+const client = mqtt.connect(mqttBrokerUrl, mqttOptions);
+
+// Event handler untuk koneksi MQTT
 client.on('connect', () => {
-    client.publish('skripsi/byhendrich/subscribe', 'Hello, HiveMQ!', (err) => {
-        if (err) {
-          console.error('Error publishing message:', err);
-        } else {
-          console.log('Message published successfully');
-        }
-    });
-})
-  
-client.subscribe('skripsi/byhendrich/publish', (err, response) => {
+  console.log('Connected to MQTT broker');
+  client.subscribe(topicSub, (err) => {
     if (err) {
-      console.error('Error subscribing to topic:', err);
+      console.error('Failed to subscribe to topic:', topicSub);
     } else {
-      console.log('Subscribed to topic successfully');
-    //   console.log(response);
+      console.log('Subscribed to topic:', topicSub);
     }
+  });
 });
 
+// Event handler untuk pesan MQTT
 client.on('message', (topic, message) => {
-    console.log(Received message on topic ${topic}: ${message});
+  console.log(`Received message: ${message.toString()} on topic: ${topic}`);
 });
 
-// client.on("connect", () => {
-//     client.subscribe("presence", (err) => {
-//       if (!err) {
-//         client.publish("presence", "Hello mqtt");
-//       }
-//     });
-//   });
-  
-// client.on("message", (topic, message) => {
-//     // message is Buffer
-//     console.log(message.toString());
-//     client.end();
-//   });
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Endpoint untuk mempublikasikan pesan ke MQTT
+app.post('/publish', (req, res) => {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).send('Message is required');
+  }
+
+  client.publish(topicPub, message, (err) => {
+    if (err) {
+      return res.status(500).send('Failed to publish message');
+    }
+
+    res.status(200).send('Message published successfully');
+  });
+});
+
+// Memulai server Express
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});

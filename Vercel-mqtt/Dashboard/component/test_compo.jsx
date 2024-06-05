1445140a626110d9ac9b7ac5f62cc3a9d310518e
hendrichhrm <hendrichherdanto16@gmail.com>
2024-06-06 05:37:37 +0700

    Update test_compo.jsx

--- Vercel-mqtt/Dashboard/component/test_compo.jsx
+++ Vercel-mqtt/Dashboard/component/test_compo.jsx
@@ -5,23 +5,18 @@ import send_message from '../../../server/apis/sendMessage';
 const TestCompo = () => {
     const [messages, setMessages] = useState([]);
     const [connected, setConnected] = useState(false);
+    const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt');
 
     useEffect(() => {
-        const option = {
-            username: 'burjoy',
-            password: 'Tester123456',
-        };
-
-        const client = mqtt.connect('wss://fd463dae44d44830b87a2ab8ad7e97f1.s1.eu.hivemq.cloud:8884/mqtt', option);
-
         client.on('connect', () => {
             console.log('Connected to broker');
             setConnected(true);
-            client.subscribe('test', { qos: 2 }, (error) => {
-                if (error) {
-                    console.error('Subscription error:', error);
-                }
-            });
+        });
+
+        client.subscribe('test', { qos: 2 }, (error) => {
+            if (error) {
+                console.error('Subscription error:', error);
+            }
         });
 
         client.on('message', (topic, message) => {
@@ -33,32 +28,22 @@ const TestCompo = () => {
             console.error('Connection error:', error);
         });
 
-        return () => {
-            if (client.connected) {
-                client.end();
-            }
-        };
     }, []);
+    console.log(messages);
+    if (messages.at(-1) == "Testing MQTT connection") {
+        const time = new Date().toLocaleString();
+        send_message(time, messages);
+    }
 
-    useEffect(() => {
-        if (messages.length > 0 && messages.at(-1) === 'Testing MQTT connection') {
-            const time = new Date().toLocaleString();
-            send_message(time, messages);
-        }
-    }, [messages]);
-
-    return ( <
-        div >
-        <
-        h1 > { connected ? 'Connected' : 'Disconnected' } < /h1> <
-        h1 > MQTT Messages < /h1> <
-        ul > {
-            messages.map((msg, index) => ( <
-                li key = { index } > { msg } < /li>
-            ))
-        } <
-        /ul> < /
-        div >
+    return (
+        <div>
+        <h1> {connected} </h1> 
+            <h1> MQTT Messages </h1> 
+            <ul>{
+            messages.map((msg, index) => (<li key={index}>{msg} </li>))
+            } 
+                </ul> 
+            </div>
     );
 };
 

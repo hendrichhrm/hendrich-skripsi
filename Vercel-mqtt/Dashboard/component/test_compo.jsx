dbad236b5f121ccd7370bddb234d9cf79c419583
hendrichhrm <hendrichherdanto16@gmail.com>
2024-06-07 11:11:06 +0700

    Update test_compo.jsx

--- Vercel-mqtt/Dashboard/component/test_compo.jsx
+++ Vercel-mqtt/Dashboard/component/test_compo.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import mqtt from 'mqtt';
-import send_message from '../../../server/apis/sendMessage';
+import send_message from '../../../server/apis/send_message';
 
 const TestCompo = () => {
     const [messages, setMessages] = useState([]);
     const [connected, setConnected] = useState(false);
-    const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt');
 
     useEffect(() => {
+        const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt');
+
         client.on('connect', () => {
             console.log('Connected to broker');
             setConnected(true);
-        });
-
-        client.subscribe('test', { qos: 2 }, (error) => {
-            if (error) {
-                console.error('Subscription error:', error);
-            }
+            client.subscribe('skripsi/byhendrich/dashtoesp', { qos: 2 }, (error) => {
+                if (error) {
+                    console.error('Subscription error:', error);
+                }
+            });
         });
 
         client.on('message', (topic, message) => {
@@ -28,22 +28,30 @@ const TestCompo = () => {
             console.error('Connection error:', error);
         });
 
+        return () => {
+            client.end();
+        };
     }, []);
-    console.log(messages);
-    if (messages.at(-1) == "Testing MQTT connection") {
-        const time = new Date().toLocaleString();
-        send_message(time, messages);
-    }
 
-    return (
-        <div>
-        <h1> {connected} </h1> 
-            <h1> MQTT Messages </h1> 
-            <ul>{
-            messages.map((msg, index) => (<li key={index}>{msg} </li>))
-            } 
-                </ul> 
-            </div>
+    useEffect(() => {
+        if (messages.length > 0 && messages.at(-1) === "Testing MQTT connection") {
+            const time = new Date().toLocaleString();
+            send_message(time, messages);
+        }
+    }, [messages]);
+
+    return ( <
+        div >
+        <
+        h1 > { connected ? 'Connected to MQTT Broker' : 'Connecting...' } < /h1> <
+        h1 > MQTT Messages < /h1> <
+        ul > {
+            messages.map((msg, index) => ( <
+                li key = { index } > { msg } < /li>
+            ))
+        } <
+        /ul> < /
+        div >
     );
 };
 
